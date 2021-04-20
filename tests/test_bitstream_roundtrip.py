97e70399672e667c795390a83dc737d53a775598
Maciej Kurc <mkurc@antmicro.com>
2021-04-20 11:13:36 +0200

    Updated round-trip tests
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- tests/test_bitstream_roundtrip.py
+++ tests/test_bitstream_roundtrip.py
@@ -8,7 +8,7 @@ import tarfile
 # =============================================================================
 
 
-def test_bitstream_roundtrip():
+def bitstream_roundtrip(bitstream_file, bitstream_format):
     """
     Bitstream -> FASM -> bitstream round-trip test
     """
@@ -19,7 +19,7 @@ def test_bitstream_roundtrip():
     with tempfile.TemporaryDirectory() as tempdir:
 
         # Unpack the bitstream
-        bitstream = os.path.join(basedir, "data", "qlf_k4n8-counter.bit.tar.gz")
+        bitstream = os.path.join(basedir, "data", bitstream_file)
         tar = tarfile.open(name=bitstream, mode="r:gz")
         tar.extractall(path=tempdir)
 
@@ -28,31 +28,34 @@ def test_bitstream_roundtrip():
         bitstream1 = os.path.join(tempdir, os.path.basename(bitstream).replace(".tar.gz", ""))
         fasm1 = os.path.join(tempdir, "fasm1.fasm")
 
-        args = "{} --db-root {} {} {}".format(
+        args = "{} --db-root {} {} {} -d -f {}".format(
             qlf_fasm,
             database,
             bitstream1,
-            fasm1
+            fasm1,
+            bitstream_format
         )
         subprocess.call(args, shell=True)
 
         # Assemble the FASM back to bitstream
         bitstream2 = os.path.join(tempdir, "bitstream2.bit")
-        args = "{} --db-root {} {} {}".format(
+        args = "{} --db-root {} {} {} -a -f {}".format(
             qlf_fasm,
             database,
             fasm1,
-            bitstream2
+            bitstream2,
+            bitstream_format
         )
         subprocess.call(args, shell=True)
 
         # Disassemble to FASM again
         fasm2 = os.path.join(tempdir, "fasm2.fasm")
-        args = "{} --db-root {} {} {}".format(
+        args = "{} --db-root {} {} {} -d -f {}".format(
             qlf_fasm,
             database,
             bitstream2,
-            fasm2
+            fasm2,
+            bitstream_format
         )
         subprocess.call(args, shell=True)
 
@@ -63,3 +66,10 @@ def test_bitstream_roundtrip():
             fasm_lines2 = fp.readlines().sort()
 
         assert fasm_lines1 == fasm_lines2
+
+
+def test_txt_bitstream_roundtrip():
+    bitstream_roundtrip("qlf_k4n8-counter.bit.tar.gz", "txt")
+
+def test_4byte_bitstream_roundtrip():
+    bitstream_roundtrip("qlf_k4n8-and.hex.tar.gz", "4byte")
